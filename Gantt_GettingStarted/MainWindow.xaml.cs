68338bbfa6133abcb0f6e734e2ac553575908ced
AzureAD\RamyaSoundarRajan <ramya.soundarrajan@syncfusion.com>
2020-07-22 20:13:46 +0530

    Added sample.

---
+++ Gantt_GettingStarted/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using Syncfusion.Windows.Controls.Gantt;
+using Syncfusion.Windows.Controls.Gantt.Chart;
+using Syncfusion.Windows.Controls.Gantt.Schedule;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gantt_GettingStarted
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GanttSchedule ganttSchedule;
+        private GanttChart ganttChart;
+        ViewModel model;
+        public MainWindow()
+        {
+            InitializeComponent();
+            model = this.DataContext as ViewModel;
+
+        }
+
+        private void Gantt_Loaded(object sender, RoutedEventArgs e)
+        {
+            Border border = VisualTreeHelper.GetChild(Gantt, 0) as Border;
+            Grid grid = VisualTreeHelper.GetChild(border, 0) as Grid;
+            ScrollViewer scrollViewer = VisualTreeHelper.GetChild(grid, 2) as ScrollViewer;
+            Grid scrollGrid = scrollViewer.Content as Grid;
+            ganttChart = VisualTreeHelper.GetChild(scrollGrid, 1) as GanttChart;
+            ganttSchedule = VisualTreeHelper.GetChild(scrollGrid, 0) as GanttSchedule;
+
+            List<StripLineInfo> stripCollection = new List<StripLineInfo>();
+            var differenceTime = (this.ganttSchedule.EndTime - this.ganttSchedule.StartTime).Days;
+            for (int i = 0; i < differenceTime; i++)
+            {
+
+
+                stripCollection.Add(new StripLineInfo()
+                {
+                    Type = StriplineType.Absolute,
+                    Width = 2,
+                    Height = this.ganttChart.ActualHeight,
+                    Position = new Point(PositionCalculation(this.ganttSchedule.StartTime.AddDays(i)), 0),
+                    HorizontalContentAlignment = HorizontalAlignment.Center,
+                    VerticalContentAlignment = VerticalAlignment.Center,
+                    Background = new SolidColorBrush(Colors.Orange),
+                });
+            }
+            model.StripCollection = stripCollection;
+        }
+
+        private double PositionCalculation(DateTime day)
+        {
+            DateTime date = ganttSchedule.StartTime;
+            MethodInfo methodInfo = typeof(GanttChart).GetMethod(
+    "GetStartPositionOfDate",
+    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static,
+    null,
+    new Type[] { typeof(DateTime) },
+    null);
+            object[] dateObjects = new object[] { day };
+            double extent =
+                (double)
+                methodInfo.Invoke(
+                    this.ganttChart,
+                    BindingFlags.Instance | BindingFlags.NonPublic,
+                    null,
+                    dateObjects,
+                    CultureInfo.CurrentCulture);
+            return extent;
+        }
+    }
+}
---
+++ Gantt_GettingStarted/ViewModels/ViewModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Syncfusion.Windows.Controls.Gantt;
+using System.Windows;
+using Syncfusion.Windows.Shared;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+using System.Reflection;
+using System.Globalization;
+using Syncfusion.Windows.Controls.Gantt.Chart;
+
+namespace Gantt_GettingStarted
+{
+    public class ViewModel :NotificationObject
+    {
+        #region Ctor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ViewModel"/> class.
+        /// </summary>
+        public ViewModel()
+        {
+            InitialSetup();
+        }
+
+        #endregion
+
+        #region Private Members
+
+        private ObservableCollection<TaskDetails> _taskCollection;
+        private ObservableCollection<Resource> _resourceCollection;
+        private List<StripLineInfo> _stripCollection;
+
+        #endregion
+
+        #region Public Members
+
+        /// <summary>
+        /// Gets or sets the appointment item source.
+        /// </summary>
+        /// <value>The appointment item source.</value>
+        public ObservableCollection<TaskDetails> TaskCollection
+        {
+            get
+            {
+                return _taskCollection;
+            }
+            set
+            {
+                _taskCollection = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the gantt resources.
+        /// </summary>
+        /// <value>The gantt resources.</value>
+        public ObservableCollection<Resource> ResourceCollection
+        {
+            get
+            {
+                return _resourceCollection;
+            }
+            set
+            {
+                _resourceCollection = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the strip collection.
+        /// </summary>
+        /// <value>The strip collection.</value>
+        public List<StripLineInfo> StripCollection
+        {
+            get
+            {
+                return _stripCollection;
+            }
+            set
+            {
+                _stripCollection = value;
+                RaisePropertyChanged("StripCollection");
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Initials the setup.
+        /// </summary>
+        private void InitialSetup()
+        {
+            TaskRepository task = new TaskRepository();
+            this._taskCollection = task.TaskCollection;
+            this.ResourceCollection = task.ResourceCollection;
+        }
+        #endregion
+    }
+}
